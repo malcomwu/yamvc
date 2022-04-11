f862e049e09dde200e829ce080a48b9c7f541518
Malcom Wu <malcomwu@gmail.com>
2022-04-11 13:25:26 +0800

    Update BaseView.js

--- src/BaseView.js
+++ src/BaseView.js
@@ -1,8 +1,17 @@
 
 export default class BaseView {
-  constructor(a?, b?) {
+  constructor(a, b) {
     this.name = 'base-view'
-    this.a? = a?
-    this.b? = b?
+    this.a = a  // ??
+    this.b = b  // ??
+  }
+
+  toString() {
+    return 'To be described..'
+  }
+
+  toJSON() {
+    const { name } = this
+    return { name }
   }
 }
