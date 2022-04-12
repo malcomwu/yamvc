eb650e421d5eb4589008fe1bbe2067fefe052026
Malcom Wu <malcomwu@gmail.com>
2022-04-12 17:37:29 +0800

    Update TodoListView.js

--- examples/TodoList/TodoListView.js
+++ examples/TodoList/TodoListView.js
@@ -1,1 +1,17 @@
+import BaseView from 'yamvc/BaseView'
+import element from 'yamvc/element'
 
+export default class TodoListView extends BaseView {
+  constructor(points) {
+    super(points)
+    this.name = 'todo-list-view'
+  }
+
+  fragment() {
+    return element(
+      'div',
+      {theAttributes},
+      [theContent]
+    )
+  }
+}
