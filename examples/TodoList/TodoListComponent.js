9b4c63bd99e6aa72f2125bfe9fe98a4cf985cee3
Malcom Wu <malcomwu@gmail.com>
2022-04-12 17:32:17 +0800

    Update TodoListComponent.js

--- examples/TodoList/TodoListComponent.js
+++ examples/TodoList/TodoListComponent.js
@@ -1,4 +1,3 @@
-import element from 'yamvc/element'
 import TodoListModel from './TodoListModel'
 import TodoListView from './TodoListView'
 import TodoListController from './TodoListController'
@@ -7,13 +6,5 @@ export default class TodoComponent extends BaseComponent {
   constructor(..) {
     super(..)
     this.name = 'todo-component'
-  }
-
-  fragment() {
-    return element(
-      'div',
-      {theAttributes},
-      [theContent]
-    )
   }
 }
