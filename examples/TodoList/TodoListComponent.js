be0684c9656ba8b86bc9f0ab214d303734510877
Malcom Wu <malcomwu@gmail.com>
2022-04-12 16:52:48 +0800

    Create TodoListComponent.js

---
+++ examples/TodoList/TodoListComponent.js
@@ -0,0 +1,10 @@
+import TodoListModel from './TodoListModel'
+import TodoListView from './TodoListView'
+import TodoListController from './TodoListController'
+
+export default class TodoComponent extends BaseComponent {
+  constructor(..) {
+    super(..)
+    this.name = 'todo-component'
+  }
+}
