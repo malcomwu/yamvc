45d5f89fd9b56ad5cefa664c72d97f49eb64157d
Malcom Wu <malcomwu@gmail.com>
2022-04-12 18:05:12 +0800

    Update element.js

--- src/element.js
+++ src/element.js
@@ -1,4 +1,38 @@
 
+export class Element {
+  constructor(tagName, attributes, content) {
+    this.name = 'element'
+    this.tagName = tagName
+    this.attributes = attributes 
+    this.content = content 
+  }
+
+  toString() {
+    // To be implemented..
+  }
+
+  toJSON() {
+    const { name, tagName, attributes, content }
+    return { name, tagName, attributes, content }
+  }
+}
+
+export class Attributes {
+  constructor(value) {
+    this.name = 'attributes'
+    this.value = value 
+  }
+
+  toString() {
+    // To be implemented..
+  }
+
+  toJSON() {
+    const { name, value } = this
+    return { name, value }
+  }
+}
+
 export default function element(name, attributes, content) {
   return {
     name, attributes, content 
